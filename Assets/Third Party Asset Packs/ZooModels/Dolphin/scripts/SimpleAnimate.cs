c4340b4125d05889d8b1834a178a4e36faae116c
jonny <jonnyfewkes@hotmail.com>
2021-11-12 21:15:33 -0700

    Added a dolphin asset pack and 2 boat asset packs.
    I added all asset packs into a folder called "Third Party Asset Packs"

---
+++ Assets/Third Party Asset Packs/ZooModels/Dolphin/scripts/SimpleAnimate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SimpleAnimate : MonoBehaviour {
+
+	private Animator anim;
+	private string AnimatorName;
+	public float CrossfadeVal = 0.25f;
+
+	private GameObject ChangeAnimationButton;
+
+	void Start () {
+		ChangeAnimationButton = GameObject.Find("ChangeAnimationButton");
+
+		anim = GetComponent<Animator> ();
+		AnimatorName = anim.name;
+		print ("name " + AnimatorName);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+	public void ChangeAnimationButtonClicked()
+	{
+		anim.CrossFade (AnimatorName +  ChangeAnimationButton.GetComponentInChildren<Text> ().text, CrossfadeVal);
+		print ("chaning Animation");
+	}
+}
