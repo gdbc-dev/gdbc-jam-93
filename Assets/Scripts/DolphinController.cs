04c99f01c9e6e29a14c8bdf8394c629b84b24168
myazuid <nick.mckenzie.uk@gmail.com>
2021-11-13 14:04:04 -0500

    made a copy scene

--- Assets/Scripts/DolphinController.cs
+++ Assets/Scripts/DolphinController.cs
@@ -23,6 +23,9 @@ public class DolphinController : MonoBehaviour
                 {
                     isUnwell = true;
                     print("This poor dolphin is now very unwell");
+
+                    // needs to remove itself from the alive dolphins
+                    // and then that list needs to inform the tourists
                 }
             }
         }
