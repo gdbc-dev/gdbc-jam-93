a631ce4211618daf0015274b3a01aa6f51c98d26
myazuid <nick.mckenzie.uk@gmail.com>
2021-11-12 23:25:46 -0500

    merged in the AI test scene and related prefabs and scripts

---
+++ Assets/Scripts/PlayerBoatsController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityMovementAI;
+
+public class PlayerBoatsController : MonoBehaviour
+{
+    // Components
+    Rigidbody2D rb;
+    SteeringBasics sb;
+    FollowPath fp;
+    WallAvoidance wa;
+    Pursue pu;
+
+    // Variables
+    public LinePath path;
+    [SerializeField] MovementAIRigidbody pursueTarget;
+
+    // Behaviour Settings
+    [SerializeField] bool pathLoop;
+    [SerializeField] bool reversePath;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sb = GetComponent<SteeringBasics>();
+        fp = GetComponent<FollowPath>();
+        wa = GetComponent<WallAvoidance>();
+        pu = GetComponent<Pursue>();
+
+        path.CalcDistances();
+    }
+
+
+    private void FixedUpdate()
+    {
+        path.Draw();
+
+        if (reversePath && fp.IsAtEndOfPath(path))
+        {
+            path.ReversePath();
+        }
+
+        var waAccel = wa.GetSteering();
+
+        
+        if (waAccel.magnitude > 0.005f)
+        {
+            sb.Steer(waAccel);
+        }
+        else if (pursueTarget != null)
+        {
+            var puAccel = pu.GetSteering(pursueTarget);
+            sb.Steer(puAccel);
+        }
+        else
+        {
+            Vector3 accel = fp.GetSteering(path, pathLoop);
+            sb.Steer(accel);
+        }
+
+        sb.LookWhereYoureGoing();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Tourist"))
+        {
+            pursueTarget = other.GetComponent<MovementAIRigidbody>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Tourist"))
+        {
+            var targetRB = other.GetComponent<MovementAIRigidbody>();
+            // if it is the target that I am already chasing, then remove it
+            if (pursueTarget == targetRB)
+            {
+                pursueTarget = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TouristBoatController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityMovementAI;
+
+public class TouristBoatController : MonoBehaviour
+{
+    // Components
+    Rigidbody2D rb;
+    SteeringBasics sb;
+    Evade evade;
+    WallAvoidance wa;
+
+    // Variables
+    [SerializeField] Transform arriveTarget;
+    [SerializeField] MovementAIRigidbody evadeTarget;
+    [SerializeField] Vector3 evadeAccel, arriveAccel, wallAvoidAccel, accel;
+    [SerializeField] float evadeThreshold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sb = GetComponent<SteeringBasics>();
+        evade = GetComponent<Evade>();
+        wa = GetComponent<WallAvoidance>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        evadeAccel = evade.GetSteering(evadeTarget);
+        arriveAccel = sb.Arrive(arriveTarget.position);
+        wallAvoidAccel = wa.GetSteering();
+
+        if (wallAvoidAccel.magnitude > 0.005f)
+        {
+            accel = wallAvoidAccel;
+        }
+        else if (evadeAccel.magnitude > evadeThreshold)
+        {
+            accel = evadeAccel;
+        }
+        else
+        {
+            accel = arriveAccel;
+        }
+        
+        sb.Steer(accel);
+        sb.LookWhereYoureGoing();
+    }
+}
